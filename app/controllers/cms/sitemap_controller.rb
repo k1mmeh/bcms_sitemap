99f01c7baf59f3053e45c3a0da583f95af690658
Kimmy Chirnside <kimmy.chirnside@gmail.com>
2010-04-25 21:37:05 +1200

    Adding documentation and hiding system sections

--- app/controllers/cms/sitemap_controller.rb
+++ app/controllers/cms/sitemap_controller.rb
@@ -1,15 +1,21 @@
 class Cms::SitemapController < Cms::BaseController
   require 'uri'
 
+  # BCMS internal before filters related to login.  This module is 100% public,
+  # so these should be skipped
   skip_before_filter :redirect_to_cms_site
   skip_before_filter :login_required
   skip_before_filter :cms_access_required
+
   before_filter :configure_if_not
 
   cattr_accessor :config
 
   def google
+    # technically there can be multiple roots so @sections is any Array
     @sections = Section.root
+    # system sections shouldn't be included
+    @ignore_sections = Section.system.map(&:id)
     @root_path = @@config['root_path']
 
     render :layout => false
@@ -17,6 +23,7 @@ class Cms::SitemapController < Cms::BaseController
 
   private
 
+  # read config from static file at /config/sitemap.yml
   def configure_if_not
     @@config ||= YAML::load(File.open("#{RAILS_ROOT}/config/sitemap.yml"))
   end
--- app/helpers/cms/sitemap_helper.rb
+++ app/helpers/cms/sitemap_helper.rb
@@ -1,19 +1,24 @@
 module Cms::SitemapHelper
 
-  def section_node(section, xml, level = 0)
-    section.child_nodes.all(:order => 'node_type ASC, position ASC').each do |node|
-      next unless ['Section', 'Page'].include? node.node_type
+  def section_node(section, xml, options = {})
+    level = options[:level] || 0
+    # don't render sections that are in the :ignore_list option
+    return if options[:ignore_list] && options[:ignore_list].include?(section.id)
+
+    section.child_nodes.all(:order => 'node_type ASC, position ASC').each do |c_node|
+      next unless ['Section', 'Page'].include? c_node.node_type
       
-      next unless node.node.public?
-      if node.node_type == 'Page'
-        page_node(node.node, xml, level)
+      next unless c_node.node.public?
+      if c_node.node_type == 'Page'
+        page_node(c_node.node, xml, :level => level)
       else
-        section_node(node.node, xml, level + 1)
+        section_node(c_node.node, xml, options.merge(:level => level + 1))
       end 
     end
   end
 
-  def page_node(page, xml, level)
+  def page_node(page, xml, options = {})
+    level = options[:level] || 0
     xml.url do
       xml.loc [@root_path.sub(/\/$/, ''), page.path.sub(/^\//, '')].join('/')
       xml.changefreq "daily"
